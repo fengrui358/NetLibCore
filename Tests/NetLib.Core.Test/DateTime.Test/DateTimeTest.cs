d677586cbea7f4fa5b18650660ad734b099e729a
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2018-02-02 10:40:56 +0800

    增加测试

---
+++ Tests/NetLib.Core.Test/DateTime.Test/DateTimeTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FrHello.NetLib.Core;
+using Xunit;
+
+namespace NetLib.Core.Test.DateTime.Test
+{
+    /// <summary>
+    /// 单元测试
+    /// </summary>
+    public class DateTimeTest
+    {
+        /// <summary>
+        /// ToUnixTimeTest
+        /// </summary>
+        [Fact]
+        public void ToUnixTimeTest()
+        {
+            var d = new System.DateTime(1975, 1, 3, 5, 3, 2, DateTimeKind.Utc).ToUnixTime();
+            Assert.Equal(157957382000L, d);
+        }
+    }
+}
