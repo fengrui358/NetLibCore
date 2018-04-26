2401feba7fde4a301b4a45eaf3eab65244ca43c0
秦晋 <qjdev@qq.com>
2018-04-27 00:23:22 +0800

    加入一些基本的转换方法

---
+++ Tests/NetLib.Core.Test/Serialization.Test/SerializationTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NetLib.Core.Test.Serialization.Test
+{
+    /// <summary>
+    /// 单元测试
+    /// </summary>
+    public class SerializationTest
+    {
+        /// <summary>
+        /// ByteToStringTest
+        /// </summary>
+        [Fact]
+        public void ByteToStringTest()
+        {
+            var s = new char[] {'a', '4'};
+            
+
+            var x = new string(s);
+            Assert.Equal("a4", x);
+        }
+    }
+}
