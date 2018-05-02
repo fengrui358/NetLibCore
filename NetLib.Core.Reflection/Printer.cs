8c5410261a806fe3777b2f65bfe37e230ced27a0
秦晋 <qjdev@qq.com>
2018-05-02 22:34:15 +0800

    临时提交

---
+++ NetLib.Core.Reflection/Printer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace FrHello.NetLib.Core.Reflection
+{
+    /// <summary>
+    /// 打印输出类型信息
+    /// </summary>
+    public static class Printer
+    {
+        public static string Output(TypeInfo type)
+        {
+            
+            return "";
+        }
+
+        public static string Output(Type type)
+        {
+
+            return "";
+        }
+
+        //public static string Output(object obj)
+        //{
+        //    return "";
+        //}
+    }
+}
---
+++ Tests/NetLib.Core.Test/Reflection.Test/PrinterTest.cs
@@ -0,0 +1,21 @@
+﻿using FrHello.NetLib.Core.Reflection;
+using Xunit;
+
+namespace NetLib.Core.Test.Reflection.Test
+{
+    public class PrinterTest
+    {
+        [Fact]
+        public void PringTypeTest()
+        {
+            var type = typeof(MockClass);
+
+            Printer.Output(type);
+        }
+    }
+
+    internal class MockClass
+    {
+        
+    }
+}
