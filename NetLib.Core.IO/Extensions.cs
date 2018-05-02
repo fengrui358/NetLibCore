b5630c16071a68a3a7d6b8a0309a25610ddc4d07
fengrui <qjdev@qq.com>
2018-05-02 08:05:58 +0800

    暂时提交

---
+++ NetLib.Core.IO/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrHello.NetLib.Core.IO
+{
+    public static class Extensions
+    {
+        public static string Read(string path)
+        {
+            return null;
+        }
+    }
+}
---
+++ NetLib.Core.IO/FileSystem.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace FrHello.NetLib.Core.IO
+{
+    public static class FileSystem
+    {
+        public static string ResolveAbsolutePath(string currentPath, string relativePath)
+        {
+            var p = Path.GetFullPath(relativePath);
+            return null;
+        }
+    }
+}
