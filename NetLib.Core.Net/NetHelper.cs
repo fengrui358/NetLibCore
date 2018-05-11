9aa3b1c6fc421973f7a094102babace99ca465cc
秦晋 <qjdev@qq.com>
2018-05-11 23:37:48 +0800

    增加新成员程序集并提交

---
+++ NetLib.Core.Net/NetHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace FrHello.NetLib.Core.Net
+{
+    /// <summary>
+    /// 网络检查工具
+    /// </summary>
+    public static class NetHelper
+    {
+        /// <summary>
+        /// Ping某个地址是否正常工作
+        /// </summary>
+        /// <param name="address">地址</param>
+        /// <returns></returns>
+        public static bool Ping(string address)
+        {
+
+
+            return false;
+        }
+
+        /// <summary>
+        /// Ping某个地址是否正常工作
+        /// </summary>
+        /// <param name="address">地址</param>
+        /// <param name="port">端口</param>
+        /// <returns></returns>
+        public static bool Ping(string address, int port)
+        {
+            return false;
+        }
+    }
+}
---
+++ Tests/NetLib.Core.Test/Net.Test/NetTest.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace NetLib.Core.Test.Net.Test
+{
+    /// <summary>
+    /// NetTest
+    /// </summary>
+    public class NetTest
+    {
+        /// <summary>
+        /// PingTest
+        /// </summary>
+        [Fact]
+        public void PingTest()
+        {
+
+        }
+
+        /// <summary>
+        /// PingPortTest
+        /// </summary>
+        [Fact]
+        public void PingPortTest()
+        {
+
+        }
+    }
+}
