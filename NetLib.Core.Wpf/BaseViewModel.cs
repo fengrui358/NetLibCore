3b40e78eb255ac9abd2b76f969e4c35a00a31a7f
秦晋 <qjdev@qq.com>
2018-05-25 00:28:39 +0800

    封装

---
+++ NetLib.Core.Wpf/BaseViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using MvvmCross;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+
+namespace FrHello.NetLib.Core.Wpf
+{
+    /// <summary>
+    /// ViewModel基类
+    /// </summary>
+    public class BaseViewModel : MvxViewModel
+    {
+        private readonly Lazy<IMvxNavigationService> _navigationService = new Lazy<IMvxNavigationService>(Mvx.Resolve<IMvxNavigationService>);
+
+        /// <summary>
+        /// 导航服务
+        /// </summary>
+        protected IMvxNavigationService NavigationService => _navigationService.Value;
+    }
+
+    /// <summary>
+    /// ViewModel基类
+    /// </summary>
+    /// <typeparam name="TParameter"></typeparam>
+    public class BaseViewModel<TParameter> : MvxViewModel<TParameter>
+    {
+        private readonly Lazy<IMvxNavigationService> _navigationService = new Lazy<IMvxNavigationService>(Mvx.Resolve<IMvxNavigationService>);
+
+        /// <summary>
+        /// 导航服务
+        /// </summary>
+        protected IMvxNavigationService NavigationService => _navigationService.Value;
+
+        /// <summary>
+        /// 准备参数
+        /// </summary>
+        /// <param name="parameter"></param>
+        public override void Prepare(TParameter parameter)
+        {
+        }
+    }
+}
---
+++ NetLib.Core.Wpf/MvxHelper.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows;
+using MvvmCross.Core;
+using MvvmCross.Platforms.Wpf.Presenters;
+using MvvmCross.Presenters;
+
+namespace FrHello.NetLib.Core.Wpf
+{
+    /// <summary>
+    /// MvvmCross框架封装辅助方法 //todo
+    /// </summary>
+    public class MvxHelper
+    {
+        /// <summary>
+        /// 在程序的Application的OnStartup方法中使用
+        /// </summary>
+        /// <param name="mainWindow">主窗体</param>
+        public static void Init(Window mainWindow)
+        {
+            Init(mainWindow, null, null);
+        }
+
+        /// <summary>
+        /// 在程序的Application的OnStartup方法中使用
+        /// </summary>
+        /// <param name="mainWindow">主窗体</param>
+        /// <param name="mvxViewPresenter">呈现控制器</param>
+        public static void Init(Window mainWindow, IMvxViewPresenter mvxViewPresenter)
+        {
+
+        }
+
+        /// <summary>
+        /// 在程序的Application的OnStartup方法中使用
+        /// </summary>
+        /// <param name="mainWindow">主窗体</param>
+        /// <param name="iMvxSetup">启动控制器</param>
+        public static void Init(Window mainWindow, IMvxSetup iMvxSetup)
+        {
+
+        }
+
+        /// <summary>
+        /// 在程序的Application的OnStartup方法中使用
+        /// </summary>
+        /// <param name="mainWindow">主窗体</param>
+        /// <param name="mvxViewPresenter">呈现控制器</param>
+        /// <param name="iMvxSetup">启动控制器</param>
+        public static void Init(Window mainWindow, IMvxViewPresenter mvxViewPresenter, IMvxSetup iMvxSetup)
+        {
+            if (mvxViewPresenter == null)
+            {
+                var mvxWpfViewPresenter = new MvxWpfViewPresenter(mainWindow);
+                mvxViewPresenter = (IMvxViewPresenter)mvxWpfViewPresenter;
+            }
+
+            if (iMvxSetup == null)
+            {
+
+            }
+        }
+    }
+}
