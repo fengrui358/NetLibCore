41a4a9b2d23f540d592103f044e256ac30a50897
秦晋 <qjdev@qq.com>
2018-05-26 08:46:44 +0800

    写初始化代码

--- NetLib.Core.Wpf/BaseSetup.cs
+++ NetLib.Core.Wpf/BaseSetup.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+using MvvmCross;
 using MvvmCross.Logging;
 using MvvmCross.Platforms.Wpf.Core;
 using MvvmCross.ViewModels;
@@ -12,10 +10,20 @@ using MvvmCross.ViewModels;
 namespace FrHello.NetLib.Core.Wpf
 {
     /// <summary>
-    /// todo:
+    /// MvxWpfSetup重写部分启动流程
     /// </summary>
-    public class BaseSetup : MvxWpfSetup
+    public class BaseSetup<TMainWindow, TViewModel> : MvxWpfSetup where TMainWindow : Window where TViewModel : BaseViewModel
     {
+        /// <summary>
+        /// 视图对应的程序集
+        /// </summary>
+        private readonly Assembly[] _viewAssemblies;
+
+        /// <summary>
+        /// ViewModel对应的程序集
+        /// </summary>
+        private readonly Assembly[] _viewModelAssemblies;
+
         /// <summary>
         /// 构造
         /// </summary>
@@ -23,12 +31,20 @@ namespace FrHello.NetLib.Core.Wpf
         /// <param name="viewModelAssemblies">ViewModel程序集</param>
         public BaseSetup(Assembly[] viewAssemblies = null, Assembly[] viewModelAssemblies = null)
         {
-
+            _viewAssemblies = viewAssemblies;
+            _viewModelAssemblies = viewModelAssemblies;
         }
 
+        /// <summary>
+        /// 创建App
+        /// </summary>
+        /// <returns></returns>
         protected override IMvxApplication CreateApp()
         {
-            return null;
+            var app = new BaseApp();
+            Mvx.RegisterSingleton<IMvxAppStart>(new BaseAppStart<TViewModel>(app));
+
+            return app;
         }
 
         /// <summary>
@@ -38,6 +54,54 @@ namespace FrHello.NetLib.Core.Wpf
         public override MvxLogProviderType GetDefaultLogProviderType()
         {
             return MvxLogProviderType.NLog;
+        }
+
+        /// <summary>
+        /// 获取视图程序集
+        /// </summary>
+        /// <returns></returns>
+        public override IEnumerable<Assembly> GetViewAssemblies()
+        {
+            if (_viewAssemblies != null)
+            {
+                var mainWindowAssembly = typeof(TMainWindow).Assembly;
+                var viewAssemblies = _viewAssemblies.ToList();
+
+                if (!viewAssemblies.Contains(mainWindowAssembly))
+                {
+                    viewAssemblies.Add(mainWindowAssembly);
+                }
+
+                return viewAssemblies;
+            }
+            else
+            {
+                return base.GetViewAssemblies();
+            }
+        }
+
+        /// <summary>
+        /// 获取ViewModel程序集
+        /// </summary>
+        /// <returns></returns>
+        public override IEnumerable<Assembly> GetViewModelAssemblies()
+        {
+            if (_viewModelAssemblies != null)
+            {
+                var firstViewModelAssembly = typeof(TViewModel).Assembly;
+                var viewModelAssemblies = _viewModelAssemblies.ToList();
+
+                if (!viewModelAssemblies.Contains(firstViewModelAssembly))
+                {
+                    viewModelAssemblies.Add(firstViewModelAssembly);
+                }
+
+                return viewModelAssemblies;
+            }
+            else
+            {
+                return base.GetViewModelAssemblies();
+            }
         }
     }
 }
--- NetLib.Core.Wpf/MvxHelper.cs
+++ NetLib.Core.Wpf/MvxHelper.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Reflection;
 using System.Windows;
+using MvvmCross;
 using MvvmCross.Platforms.Wpf.Core;
 using MvvmCross.Platforms.Wpf.Presenters;
-using MvvmCross.Presenters;
 using MvvmCross.ViewModels;
 
 namespace FrHello.NetLib.Core.Wpf
@@ -16,27 +16,17 @@ namespace FrHello.NetLib.Core.Wpf
         /// <summary>
         /// 在程序的Application的OnStartup方法中使用
         /// </summary>
-        /// <param name="mainWindow">主窗体</param>
-        /// <param name="firstViewModel">第一个视图</param>
         /// <param name="mvxViewPresenter">呈现控制器</param>
         /// <param name="mvxSetup">启动流程控制器</param>
         /// <param name="mvxApplication">ViewModelCore当中的</param>
         /// <param name="mvxAppStart">程序启动控制</param>
         /// <param name="viewAssemblies">视图程序集</param>
         /// <param name="viewModelAssemblies">ViewModel程序集</param>
-        public static void Init(Window mainWindow, MvxViewModel firstViewModel, IMvxViewPresenter mvxViewPresenter = null,
+        public static void Init<TMainWindow, TViewModel>(MvxWpfViewPresenter mvxViewPresenter = null,
             IMvxWpfSetup mvxSetup = null, IMvxApplication mvxApplication = null, IMvxAppStart mvxAppStart = null, Assembly[] viewAssemblies = null,
-            Assembly[] viewModelAssemblies = null)
+            Assembly[] viewModelAssemblies = null) where TMainWindow : Window where TViewModel : BaseViewModel
         {
-            if (mainWindow == null)
-            {
-                throw new ArgumentNullException(nameof(mainWindow));
-            }
-
-            if (firstViewModel == null)
-            {
-                throw new ArgumentNullException(nameof(firstViewModel));
-            }
+            var mainWindow = Activator.CreateInstance<TMainWindow>();
 
             if (mvxViewPresenter == null)
             {
@@ -45,14 +35,17 @@ namespace FrHello.NetLib.Core.Wpf
 
             if (mvxSetup == null)
             {
-                mvxSetup = new BaseSetup();
+                mvxSetup = new BaseSetup<TMainWindow, TViewModel>();
             }
 
-            mvxSetup.PlatformInitialize(Application.Current.Dispatcher, mainWindow);
+            mvxSetup.PlatformInitialize(Application.Current.Dispatcher, mvxViewPresenter);
             mvxSetup.InitializePrimary();
             mvxSetup.InitializeSecondary();
 
+            var start = Mvx.Resolve<IMvxAppStart>();
+            start.Start();
 
+            mainWindow.Show();
         }
     }
 }
