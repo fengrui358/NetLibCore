292f7a8d2bf59d31576380aea34ca04e1419fd23
fengrui <fengrui@ceiec.com.cn>
2018-07-11 15:12:16 +0800

    auto commit

---
+++ NetLib.Core.Wpf/Behaviors/ItemsControlMouseDoubleClickBehavior.cs
@@ -0,0 +1,97 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+using FrHello.NetLib.Core.Wpf.Helpers;
+
+namespace FrHello.NetLib.Core.Wpf.Behaviors
+{
+    /// <summary>
+    /// 集合控件鼠标双击，发送双击对象到后端绑定命令
+    /// </summary>
+    public class ItemsControlMouseDoubleClickBehavior : Behavior<ItemsControl>
+    {
+        /// <summary>
+        /// 命令
+        /// </summary>
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register(nameof(Command), typeof(ICommand),
+                typeof(ItemsControlMouseDoubleClickBehavior),
+                new FrameworkPropertyMetadata(null));
+
+        /// <summary>
+        /// 命令
+        /// </summary>
+        public ICommand Command
+        {
+            get => (ICommand) GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        /// <summary>
+        /// 命令参数
+        /// </summary>
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.Register(nameof(CommandParameter), typeof(object),
+                typeof(ItemsControlMouseDoubleClickBehavior),
+                new FrameworkPropertyMetadata((object) null));
+
+        /// <summary>
+        /// 命令参数
+        /// </summary>
+        public object CommandParameter
+        {
+            get => GetValue(CommandParameterProperty);
+            set => SetValue(CommandParameterProperty, value);
+        }
+
+        /// <summary>
+        /// 绑定事件
+        /// </summary>
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.MouseLeftButtonDown += AssociatedObjectOnMouseLeftButtonDown;
+        }
+
+        /// <summary>
+        /// 清理
+        /// </summary>
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            AssociatedObject.MouseLeftButtonDown -= AssociatedObjectOnMouseLeftButtonDown;
+        }
+
+        /// <summary>
+        /// 鼠标点击事件处理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AssociatedObjectOnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Command != null && e.ClickCount == 2)
+            {
+                var element = e.MouseDevice.DirectlyOver;
+                if (element is FrameworkElement frameworkElement && sender is ItemsControl itemsControl)
+                {
+                    //判断是否是点击的某一项
+                    var selectedItem = itemsControl.GetType().GetProperty(nameof(Selector.SelectedItem))
+                        ?.GetValue(itemsControl);
+                    if (selectedItem != null)
+                    {
+                        //判断是否是单击的某一项
+                        var clickUiItem = itemsControl.ItemContainerGenerator.ContainerFromItem(selectedItem);
+                        if (UiTreeHelper.IsChild(clickUiItem, frameworkElement, SearchTreeType.Visual))
+                        {
+                            Command?.Execute(CommandParameter ?? selectedItem);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ NetLib.Core.Wpf/Helpers/UiTreeHelper.cs
@@ -0,0 +1,74 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace FrHello.NetLib.Core.Wpf.Helpers
+{
+    /// <summary>
+    /// Ui树的辅助类
+    /// </summary>
+    public static class UiTreeHelper
+    {
+        /// <summary>
+        /// 判断一个元素是否是另一个元素的本身或子元素
+        /// </summary>
+        /// <param name="parent">父元素</param>
+        /// <param name="child">子元素</param>
+        /// <param name="searchTreeType">搜索类型</param>
+        /// <returns></returns>
+        public static bool IsChild(DependencyObject parent, DependencyObject child,
+            SearchTreeType searchTreeType = SearchTreeType.Logic)
+        {
+            if (parent == null || child == null)
+            {
+                return false;
+            }
+
+            if (Equals(parent, child))
+            {
+                return true;
+            }
+
+            if (searchTreeType == SearchTreeType.Logic)
+            {
+                foreach (var childItem in LogicalTreeHelper.GetChildren(parent))
+                {
+                    if (childItem is DependencyObject dependencyObject &&
+                        IsChild(dependencyObject, child, searchTreeType))
+                    {
+                        return true;
+                    }
+                }
+            }
+            else if (searchTreeType == SearchTreeType.Visual)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+                {
+                    var childItem = VisualTreeHelper.GetChild(parent, i);
+                    if (childItem is DependencyObject dependencyObject &&
+                        IsChild(dependencyObject, child, searchTreeType))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// 查找模式
+    /// </summary>
+    public enum SearchTreeType
+    {
+        /// <summary>
+        /// 逻辑树
+        /// </summary>
+        Logic,
+
+        /// <summary>
+        /// 可视化树
+        /// </summary>
+        Visual
+    }
+}
