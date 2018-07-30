01dc3fb1c2859230d46b1691e64b70325862369c
fengrui <fengrui@ceiec.com.cn>
2018-07-30 15:14:36 +0800

    增加BaseEditViewModel

---
+++ NetLib.Core.Mvx/BaseEditViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System.Threading.Tasks;
+using MvvmCross.Commands;
+
+namespace FrHello.NetLib.Core.Mvx
+{
+    /// <summary>
+    /// 可编辑的ViewModel
+    /// </summary>
+    public abstract class BaseEditViewModel : BaseViewModel
+    {
+        private bool _canConfirm = true;
+
+        /// <summary>
+        /// 确认命令
+        /// </summary>
+        public MvxCommand ConfirmCommand { get; private set; }
+
+        /// <summary>
+        /// 取消命令
+        /// </summary>
+        public MvxCommand CancelCommand { get; private set; }
+
+        /// <summary>
+        /// 是否可确认
+        /// </summary>
+        public virtual bool CanConfirm
+        {
+            get => _canConfirm && CanConfirmFun();
+            set
+            {
+                if (_canConfirm != value)
+                {
+                    _canConfirm = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 构造
+        /// </summary>
+        protected BaseEditViewModel()
+        {
+            ConfirmCommand = new MvxCommand(ConfirmCommandHandler);
+            CancelCommand = new MvxCommand(Close);
+        }
+
+        /// <summary>
+        /// 确认
+        /// </summary>
+        /// <returns></returns>
+        protected abstract Task<bool> Confirm();
+
+        /// <summary>
+        /// 是否可确认(由CanConfirm的通知触发)
+        /// </summary>
+        /// <returns></returns>
+        protected virtual bool CanConfirmFun()
+        {
+            return true;
+        }
+
+        private async void ConfirmCommandHandler()
+        {
+            if (await Confirm())
+            {
+                Close();
+            }
+        }
+    }
+}
