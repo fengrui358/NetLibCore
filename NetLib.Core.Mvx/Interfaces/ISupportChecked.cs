85525dcda52d81612f0d383afb3aae1f682013a8
fengrui <fengrui@ceiec.com.cn>
2018-08-02 17:21:05 +0800

    优化MVX类

---
+++ NetLib.Core.Mvx/Interfaces/ISupportChecked.cs
@@ -0,0 +1,13 @@
+﻿namespace FrHello.NetLib.Core.Mvx.Interfaces
+{
+    /// <summary>
+    /// 是否支持复选选中
+    /// </summary>
+    public interface ISupportChecked
+    {
+        /// <summary>
+        /// 是否可以选中
+        /// </summary>
+        bool IsChecked { get; set; }
+    }
+}
---
+++ NetLib.Core.Mvx/UiDispatcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MvvmCross.Base;
+
+namespace FrHello.NetLib.Core.Mvx
+{
+    /// <summary>
+    /// 主线程辅助类
+    /// </summary>
+    public class UiDispatcherHelper
+    {
+        private static IMvxMainThreadAsyncDispatcher _mainThreadAsyncDispatcher;
+        private static readonly object Async = new object();
+
+        /// <summary>
+        /// Ui主线程调用
+        /// </summary>
+        /// <param name="action">执行方法</param>
+        public static void Invoke(Action action)
+        {
+            if (action == null) return;
+            if (_mainThreadAsyncDispatcher == null)
+            {
+                lock (Async)
+                {
+                    if (_mainThreadAsyncDispatcher == null)
+                    {
+                        _mainThreadAsyncDispatcher = MvvmCross.Mvx.Resolve<IMvxMainThreadAsyncDispatcher>();
+                    }
+                }
+            }
+
+            _mainThreadAsyncDispatcher.ExecuteOnMainThreadAsync(action);
+        }
+    }
+}
