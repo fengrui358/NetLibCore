5e09f6006b9d929300492c57f190064cf9563b9a
free <devqj@qq.com>
2018-11-23 11:23:05 -0400

    增加局域网登陆授权

---
+++ NetLib.Core.IO/IdentityScope.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace FrHello.NetLib.Core.IO
+{
+    /// <summary>
+    /// 登陆局域网认证授权
+    /// <example>
+    /// using (new IdentityScope(Config.Instance.RemoteDirectory.UserName,
+    ///     Config.Instance.RemoteDirectory.RemoteIpAddress, Config.Instance.RemoteDirectory.Password))
+    /// {
+    ///     File.Copy(zipFilePath,
+    ///     Path.Combine(Config.Instance.RemoteDirectory.RemoteFullPath, zipFileName));
+    /// }
+    /// </example>
+    /// </summary>
+    public class IdentityScope : IDisposable
+    {
+        /// <summary>
+        /// obtains user token
+        /// </summary>
+        /// <param name="pszUsername"></param>
+        /// <param name="pszDomain"></param>
+        /// <param name="pszPassword"></param>
+        /// <param name="dwLogonType"></param>
+        /// <param name="dwLogonProvider"></param>
+        /// <param name="phToken"></param>
+        /// <returns></returns>
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool LogonUser(string pszUsername, string pszDomain, string pszPassword,
+            int dwLogonType, int dwLogonProvider, ref IntPtr phToken);
+
+        /// <summary>
+        /// closes open handes returned by LogonUser
+        /// </summary>
+        /// <param name="handle"></param>
+        /// <returns></returns>
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        private static extern bool CloseHandle(IntPtr handle);
+
+        [DllImport("Advapi32.DLL")]
+        private static extern bool ImpersonateLoggedOnUser(IntPtr hToken);
+
+        [DllImport("Advapi32.DLL")]
+        private static extern bool RevertToSelf();
+
+        private const int Logon32ProviderDefault = 0;
+
+        /// <summary>
+        /// 域控中的需要用:Interactive = 2
+        /// </summary>
+        private const int Logon32LogonNewcredentials = 9;
+
+        private bool _disposed;
+
+        public IdentityScope(string sUsername, string sDomain, string sPassword)
+        {
+            // initialize tokens  
+            IntPtr pExistingTokenHandle = new IntPtr(0);
+            IntPtr pDuplicateTokenHandle = new IntPtr(0);
+
+            try
+            {
+                // get handle to token  
+                bool bImpersonated = LogonUser(sUsername, sDomain, sPassword,
+                    Logon32LogonNewcredentials, Logon32ProviderDefault, ref pExistingTokenHandle);
+
+                if (bImpersonated)
+                {
+                    if (!ImpersonateLoggedOnUser(pExistingTokenHandle))
+                    {
+                        int nErrorCode = Marshal.GetLastWin32Error();
+                        throw new Exception("ImpersonateLoggedOnUser error;Code=" + nErrorCode);
+                    }
+                }
+                else
+                {
+                    int nErrorCode = Marshal.GetLastWin32Error();
+                    throw new Exception("LogonUser error;Code=" + nErrorCode);
+                }
+            }
+            finally
+            {
+                // close handle(s)  
+                if (pExistingTokenHandle != IntPtr.Zero)
+                    CloseHandle(pExistingTokenHandle);
+                if (pDuplicateTokenHandle != IntPtr.Zero)
+                    CloseHandle(pDuplicateTokenHandle);
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                RevertToSelf();
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+    }
+}
