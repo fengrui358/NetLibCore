a44092cec21b7e74a54fa0a919246ff46b78bfc1
free <devqj@qq.com>
2019-01-10 15:56:46 -0400

    加入Json序列化的辅助类

---
+++ NetLib.Core.Serialization/JsonExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+namespace FrHello.NetLib.Core.Serialization
+{
+    /// <summary>
+    /// Json转换的一些辅助方法
+    /// </summary>
+    public static class JsonSerializationExtensions
+    {
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="json">json文本</param>
+        /// <returns>对象</returns>
+        public static T DeserializeJson<T>(this string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        /// <summary>
+        /// 对象序列化
+        /// </summary>
+        /// <typeparam name="T">需要序列化的对象类型</typeparam>
+        /// <param name="obj">需要序列化的对象</param>
+        /// <returns>Xml字符串</returns>
+        public static string SerializeJson<T>(this T obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
--- NetLib.Core.Serialization/XmlExtensions.cs
+++ NetLib.Core.Serialization/XmlExtensions.cs
@@ -13,9 +13,9 @@ namespace FrHello.NetLib.Core.Serialization
         /// <summary>
         /// 反序列化
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="xml"></param>
-        /// <returns></returns>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="xml">xml文本</param>
+        /// <returns>对象</returns>
         public static T DeserializeXml<T>(this string xml)
         {
             var xs = new XmlSerializer(typeof(T));
@@ -31,7 +31,7 @@ namespace FrHello.NetLib.Core.Serialization
         /// </summary>
         /// <typeparam name="T">需要序列化的对象类型</typeparam>
         /// <param name="obj">需要序列化的对象</param>
-        /// <param name="omitXmlDeclaration">忽略申明</param>
+        /// <param name="omitXmlDeclaration">是否忽略文件头顶部的申明</param>
         /// <returns>Xml字符串</returns>
         public static string SerializeXml<T>(this T obj, bool omitXmlDeclaration = false)
         {
