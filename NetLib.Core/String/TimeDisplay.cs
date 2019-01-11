dda06c37858c9c9caa9caabc7b43a3b99b09db4c
free <devqj@qq.com>
2019-01-11 16:30:17 -0400

    增加时间显示

---
+++ NetLib.Core/String/TimeDisplay.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// ReSharper disable once CheckNamespace
+namespace FrHello.NetLib.Core
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// 时间显示格式化
+        /// </summary>
+        /// <param name="timeSpan">时间值</param>
+        /// <returns>格式化后的字符串hh:mm:ss(mm:ss)</returns>
+        public static string TimeSpanFormat(this TimeSpan timeSpan)
+        {
+            if (timeSpan.Hours > 0)
+            {
+                return $"{timeSpan.Hours:d2}:{timeSpan.Minutes:d2}:{timeSpan.Seconds:d2}";
+            }
+
+            return $"{(int) timeSpan.Minutes:d2}:{timeSpan.Seconds:d2}";
+        }
+
+        
+    }
+
+    public static class TimeSpanExtensions
+    {
+        /// <summary>
+        /// 时间显示格式化
+        /// </summary>
+        /// <param name="seconds">用秒统计的时间总数</param>
+        /// <returns>格式化后的字符串hh:mm:ss(mm:ss)</returns>
+        public static string TimeSpanFormat(double seconds)
+        {
+            var timeSpan = new TimeSpan(0, 0, 0, (int)Math.Round(seconds));
+            return timeSpan.TimeSpanFormat();
+        }
+    }
+}
