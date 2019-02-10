39484cbb6413b55ba6c87a3135492e1a3f3344b8
free <devqj@qq.com>
2019-02-10 10:06:22 -0400

    增加一个比较器

---
+++ NetLib.Core/Comparers/ReverseComparer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace FrHello.NetLib.Core.Comparers
+{
+    /// <summary>
+    /// 用于进行反向比较
+    /// </summary>
+    /// <example>
+    /// Array.Sort(list, new ReverseComparer());
+    /// list.Sort(new ReverseComparer());
+    /// </example>
+    /// <typeparam name="T"></typeparam>
+    public class ReverseComparer<T> : IComparer<T>
+    {
+        /// <summary>
+        /// 比较
+        /// </summary>
+        /// <param name="x">x</param>
+        /// <param name="y">y</param>
+        /// <returns>比较结果</returns>
+        public int Compare(T x, T y)
+        {
+            return Comparer<T>.Default.Compare(y, x);
+        }
+    }
+}
