7af76895dacea882c5f03564de6dd42ff5e3c88a
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2019-08-12 15:34:08 +0800

    优化邮件发送

--- NetLib.Core.Net/Mail/MailHelper.cs
+++ NetLib.Core.Net/Mail/MailHelper.cs
@@ -35,7 +35,7 @@ namespace FrHello.NetLib.Core.Net
                 BodyEncoding = GlobalMailOptions.DefaultEncoding
             };
 
-            foreach (var address in toAddress.Split(new []{','}, StringSplitOptions.RemoveEmptyEntries))
+            foreach (var address in toAddress.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
             {
                 mailMessage.To.Add(new MailAddress(address));
             }
@@ -74,12 +74,17 @@ namespace FrHello.NetLib.Core.Net
             using (var smtpClient = new SmtpClient())
             {
                 smtpClient.Host = GlobalMailOptions.SmtpServerInfo.SmtpHost;
+                smtpClient.EnableSsl = GlobalMailOptions.EnableSsl;
+
                 if (GlobalMailOptions.SmtpServerInfo.Port != null)
                 {
                     smtpClient.Port = GlobalMailOptions.SmtpServerInfo.Port.Value;
                 }
+                else if (smtpClient.EnableSsl)
+                {
+                    smtpClient.Port = 587;
+                }
 
-                smtpClient.EnableSsl = GlobalMailOptions.EnableSsl;
                 smtpClient.UseDefaultCredentials = false;
                 smtpClient.Credentials = new NetworkCredential(GlobalMailOptions.SmtpServerInfo.MailUserName,
                     GlobalMailOptions.SmtpServerInfo.MailPassword);
@@ -150,12 +155,17 @@ namespace FrHello.NetLib.Core.Net
             using (var smtpClient = new SmtpClient())
             {
                 smtpClient.Host = GlobalMailOptions.SmtpServerInfo.SmtpHost;
+                smtpClient.EnableSsl = GlobalMailOptions.EnableSsl;
+
                 if (GlobalMailOptions.SmtpServerInfo.Port != null)
                 {
                     smtpClient.Port = GlobalMailOptions.SmtpServerInfo.Port.Value;
                 }
+                else if (smtpClient.EnableSsl)
+                {
+                    smtpClient.Port = 587;
+                }
 
-                smtpClient.EnableSsl = GlobalMailOptions.EnableSsl;
                 smtpClient.UseDefaultCredentials = false;
                 smtpClient.Credentials = new NetworkCredential(GlobalMailOptions.SmtpServerInfo.MailUserName,
                     GlobalMailOptions.SmtpServerInfo.MailPassword);
