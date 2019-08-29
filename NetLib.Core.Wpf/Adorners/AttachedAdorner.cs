fb6835be2ab88d35c7dfb3cfc9147a984f99e5c3
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2019-08-29 17:13:35 +0800

    增加Adorner便捷传入的方式

---
+++ NetLib.Core.Wpf/Adorners/AttachedAdorner.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+using Size = System.Windows.Size;
+
+namespace FrHello.NetLib.Core.Wpf.Adorners
+{
+    /// <summary>
+    /// 附加到其他元素上面的Adorner
+    /// </summary>
+    public class AttachedAdorner : Adorner
+    {
+        /// <summary>
+        /// 附加到其他元素上面的UIElement
+        /// </summary>
+        protected UIElement AttachedAdornerElement;
+
+        /// <summary>
+        /// 构造
+        /// </summary>
+        /// <param name="attachedAdornerElement">附加到其他元素上面的UIElement</param>
+        /// <param name="adornedTargetElement">需要附加的目标UIElement</param>
+        public AttachedAdorner(UIElement attachedAdornerElement, UIElement adornedTargetElement) : this(
+            adornedTargetElement)
+        {
+            AttachedAdornerElement = attachedAdornerElement;
+        }
+
+        /// <summary>
+        /// 构造
+        /// </summary>
+        /// <param name="adornedTargetElement">需要附加的目标UIElement</param>
+        protected AttachedAdorner(UIElement adornedTargetElement) : base(adornedTargetElement)
+        {
+            AddLogicalChild(AttachedAdornerElement);
+            AddVisualChild(AttachedAdornerElement);
+        }
+
+        /// <summary>
+        /// MeasureOverride
+        /// </summary>
+        /// <param name="constraint">constraint</param>
+        /// <returns></returns>
+        protected override Size MeasureOverride(Size constraint)
+        {
+            AttachedAdornerElement.Measure(constraint);
+            return AttachedAdornerElement.DesiredSize;
+        }
+
+        /// <summary>
+        /// ArrangeOverride
+        /// </summary>
+        /// <param name="finalSize">finalSize</param>
+        /// <returns></returns>
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            var desiredSizeWidth = AttachedAdornerElement.DesiredSize.Width;
+            var desiredSizeHeight = AttachedAdornerElement.DesiredSize.Height + 5;
+
+            //放在目标元素上面
+            AttachedAdornerElement.Arrange(new Rect(AdornedElement.RenderSize.Width / 2 - desiredSizeWidth / 2,
+                0 - desiredSizeHeight, desiredSizeWidth, desiredSizeHeight));
+
+            return finalSize;
+        }
+
+        /// <summary>
+        /// VisualChildrenCount
+        /// </summary>
+        protected override int VisualChildrenCount { get; } = 1;
+
+        /// <summary>
+        /// GetVisualChild
+        /// </summary>
+        /// <param name="index">index</param>
+        /// <returns></returns>
+        protected override Visual GetVisualChild(int index)
+        {
+            return AttachedAdornerElement;
+        }
+
+        /// <summary>
+        /// LogicalChildren
+        /// </summary>
+        protected override IEnumerator LogicalChildren
+        {
+            get
+            {
+                var list = new ArrayList { AttachedAdornerElement };
+                return list.GetEnumerator();
+            }
+        }
+
+        /// <summary>
+        /// DisconnectChild
+        /// </summary>
+        public void DisconnectChild()
+        {
+            RemoveLogicalChild(AttachedAdornerElement);
+            RemoveVisualChild(AttachedAdornerElement);
+        }
+    }
+}
---
+++ NetLib.Core.Wpf/Behaviors/AdornerBehavior.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Documents;
+using FrHello.NetLib.Core.Wpf.Adorners;
+
+namespace FrHello.NetLib.Core.Wpf.Behaviors
+{
+    /// <summary>
+    /// AdornerBehavior
+    /// </summary>
+    public class AdornerBehavior
+    {
+        /// <summary>
+        /// AdornerProperty
+        /// </summary>
+        public static readonly DependencyProperty AdornerProperty = DependencyProperty.RegisterAttached(
+            "Adorner", typeof(UIElement), typeof(AdornerBehavior),
+            new PropertyMetadata(default(UIElement), OnAdornerChangedCallback));
+
+        /// <summary>
+        /// GetAdorner
+        /// </summary>
+        /// <param name="element">需要附加的目标UIElement</param>
+        /// <returns></returns>
+        public static UIElement GetAdorner(UIElement element)
+        {
+            if (element == null)
+            {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            return (UIElement)element.GetValue(AdornerProperty);
+        }
+
+        /// <summary>
+        /// SetAdorner
+        /// </summary>
+        /// <param name="element">需要附加的目标UIElement</param>
+        /// <param name="value">附加到其他元素上面的UIElement</param>
+        public static void SetAdorner(UIElement element, UIElement value)
+        {
+            if (element == null)
+            {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            element.SetValue(AdornerProperty, value);
+        }
+
+        /// <summary>
+        /// OnAdornerChangedCallback
+        /// </summary>
+        /// <param name="dependencyObject">dependencyObject</param>
+        /// <param name="args">args</param>
+        private static void OnAdornerChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+        {
+            if (dependencyObject is UIElement element)
+            {
+                var uiElement = (UIElement)args.NewValue;
+
+                var adornerLayer = AdornerLayer.GetAdornerLayer(element);
+
+                if (adornerLayer != null)
+                {
+                    var adorners = adornerLayer?.GetAdorners(element);
+
+                    if (adorners != null)
+                    {
+                        foreach (var adorner in adorners)
+                        {
+                            if (adorner is AttachedAdorner oldAttachedAdorner)
+                            {
+                                adornerLayer.Remove(adorner);
+                                oldAttachedAdorner.DisconnectChild();
+                            }
+                        }
+                    }
+
+                    if (uiElement != null)
+                    {
+                        adornerLayer.Add(new AttachedAdorner(uiElement, element));
+                    }
+                }
+            }
+        }
+    }
+}
