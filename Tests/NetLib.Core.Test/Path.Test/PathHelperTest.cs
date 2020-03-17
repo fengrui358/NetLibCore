64bbd2c10fa3a49f0f2116d189e6a069d57b6a4b
free <devqj@qq.com>
2020-03-18 00:28:27 +0800

    增加pathhelper用于拼装Url

---
+++ Tests/NetLib.Core.Test/Path.Test/PathHelperTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FrHello.NetLib.Core.Helpers;
+using Xunit;
+
+namespace NetLib.Core.Test.Path.Test
+{
+    /// <summary>
+    /// PathHelperTest
+    /// </summary>
+    public class PathHelperTest
+    {
+        /// <summary>
+        /// PathHelperTest
+        /// </summary>
+        [Fact]
+        public void UrlCombineTest()
+        {
+            var actual1 = "10.45.32.56:4353/dfsfsa/gfgd";
+            var excepted1 = PathHelper.UrlCombine(string.Empty, "/10.45.32.56:4353", "/dfSfsa/gfGd");
+            Assert.Equal(excepted1, actual1);
+
+            var actual2 = "https://cd.bendibao.com/live/201784/91973.shtm";
+            var excepted2 = PathHelper.UrlCombine("https", "cd.bendibao.com\\live/", "/201784/91973.shtm");
+            Assert.Equal(excepted2, actual2);
+        }
+    }
+}
