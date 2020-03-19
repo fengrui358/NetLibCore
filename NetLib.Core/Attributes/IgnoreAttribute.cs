c332e6f2f96c1c2d15217da3d11f63e01a1a3180
free <devqj@qq.com>
2020-03-20 00:16:46 +0800

    增加忽略列

---
+++ NetLib.Core/Attributes/IgnoreAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FrHello.NetLib.Core.Attributes
+{
+    /// <summary>
+    /// 忽略
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class IgnoreAttribute : Attribute
+    {
+    }
+}
