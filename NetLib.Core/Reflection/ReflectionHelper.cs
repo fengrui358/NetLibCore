8155a4a6f2c98e37d58549711fea7d29f4f4f20b
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2020-05-15 10:40:25 +0800

    增加一个反射功能

---
+++ NetLib.Core/Reflection/ReflectionHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace FrHello.NetLib.Core.Reflection
+{
+    /// <summary>
+    /// 反射辅助类
+    /// </summary>
+    public static class ReflectionHelper
+    {
+        /// <summary>
+        /// 获取方法调用者信息
+        /// </summary>
+        /// <param name="className"></param>
+        /// <param name="methodName"></param>
+        public static void GetInvokerInfo(out string className, out string methodName)
+        {
+            var trace = new StackTrace();
+            var frame = trace.GetFrame(2);//1代表上级，2代表上上级，以此类推
+            var method = frame.GetMethod();
+
+            methodName = method.Name;
+            className = method.ReflectedType?.Name;
+        }
+    }
+}
---
+++ Tests/NetLib.Core.Test/Reflection.Test/ReflectionHelperTest.cs
@@ -0,0 +1,33 @@
+﻿using FrHello.NetLib.Core.Reflection;
+using Xunit;
+
+namespace NetLib.Core.Test.Reflection.Test
+{
+    /// <summary>
+    /// ReflectionHelperTest
+    /// </summary>
+    public class ReflectionHelperTest
+    {
+        /// <summary>
+        /// GetInvokerInfoTest
+        /// </summary>
+        [Fact]
+        public void GetInvokerInfoTest()
+        {
+            Assert.Equal(nameof(ReflectionHelperTest), GetClassName());
+            Assert.Equal(nameof(ReflectionHelperTest.GetInvokerInfoTest), GetMethodName());
+        }
+
+        private string GetClassName()
+        {
+            ReflectionHelper.GetInvokerInfo(out var className, out var methodName);
+            return className;
+        }
+
+        private string GetMethodName()
+        {
+            ReflectionHelper.GetInvokerInfo(out var className, out var methodName);
+            return methodName;
+        }
+    }
+}
