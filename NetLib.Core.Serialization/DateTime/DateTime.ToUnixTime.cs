122e9a36bdbcc5c0bc664909225d68529a87abc8
fengrui <fengrui358@163.com>
2021-10-28 15:03:04 +0800

    修改注释

--- NetLib.Core.Serialization/DateTime/DateTime.ToUnixTime.cs
+++ NetLib.Core.Serialization/DateTime/DateTime.ToUnixTime.cs
@@ -10,7 +10,7 @@ namespace FrHello.NetLib.Core.Serialization.DateTime
         private static readonly System.DateTime Time19700101 = new System.DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
 
         /// <summary>
-        /// Unix 时间戳是从格林威治时间1970 年 01 月 01 日 00 时 00 分 00 秒(北京时间 1970 年 01 月 01 日 08 时 00 分 00 秒)起至现在的总秒数
+        /// Unix 时间戳是从格林威治时间1970 年 01 月 01 日 00 时 00 分 00 秒(北京时间 1970 年 01 月 01 日 08 时 00 分 00 秒)起至现在的总毫秒数
         /// </summary>
         /// <param name="time">要转换的时间</param>
         /// <returns>时间戳</returns>
@@ -21,7 +21,7 @@ namespace FrHello.NetLib.Core.Serialization.DateTime
         }
 
         /// <summary>
-        /// Unix 时间戳是从格林威治时间1970 年 01 月 01 日 00 时 00 分 00 秒(北京时间 1970 年 01 月 01 日 08 时 00 分 00 秒)起至现在的总秒数
+        /// Unix 时间戳是从格林威治时间1970 年 01 月 01 日 00 时 00 分 00 秒(北京时间 1970 年 01 月 01 日 08 时 00 分 00 秒)起至现在的总毫秒数
         /// </summary>
         /// <param name="unixTimestamp">时间戳</param>
         /// <returns>转换回的时间</returns>
